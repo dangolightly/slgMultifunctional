a48c47c8cb0f5be4cee7b1a07ddf94aa67ce0503
dgol321 <30943887+dangolightly@users.noreply.github.com>
2020-04-24 08:36:51 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace slgMultifunctional.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
